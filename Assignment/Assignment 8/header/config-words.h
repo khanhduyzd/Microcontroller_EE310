8a1ae84d96a40f8a7c29bd27ceae1d1eb3823d7e
khanhduyzd <zilky11@gmail.com>
2026-04-16 11:26:17 -0700

    Update config-words.h

--- Assignment/Assignment 8/header/config-words.h
+++ Assignment/Assignment 8/header/config-words.h
@@ -1,1 +1,47 @@
+#ifndef CONFIG_WORDS_H
+#define CONFIG_WORDS_H
 
+#include <xc.h>
+
+// CONFIG1
+#pragma config FEXTOSC = OFF
+#pragma config RSTOSC = HFINTOSC_1MHZ
+#pragma config CLKOUTEN = OFF
+#pragma config CSWEN = ON
+#pragma config FCMEN = OFF
+
+// CONFIG2
+#pragma config MCLRE = EXTMCLR
+#pragma config PWRTS = PWRT_OFF
+#pragma config MVECEN = ON
+#pragma config IVT1WAY = ON
+#pragma config LPBOREN = OFF
+#pragma config BOREN = SBORDIS
+#pragma config BORV = VBOR_2P45
+#pragma config ZCD = OFF
+#pragma config PPS1WAY = ON
+#pragma config STVREN = ON
+#pragma config DEBUG = OFF
+#pragma config XINST = OFF
+
+// CONFIG3
+#pragma config WDTCPS = WDTCPS_31
+#pragma config WDTE = OFF
+#pragma config WDTCWS = WDTCWS_7
+#pragma config WDTCCS = SC
+
+// CONFIG4
+#pragma config BBSIZE = BBSIZE_512
+#pragma config BBEN = OFF
+#pragma config SAFEN = OFF
+#pragma config WRTAPP = OFF
+#pragma config WRTB = OFF
+#pragma config WRTC = OFF
+#pragma config WRTD = OFF
+#pragma config WRTSAF = OFF
+#pragma config LVP = OFF
+
+// CONFIG5
+#pragma config CP = OFF
+
+#endif
